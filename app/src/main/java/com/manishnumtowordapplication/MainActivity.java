d2ebe653861266a9caf0aea7f6eb709fe5077ef8
manishmhr2@gmail.com <manishmhr2.com>
2019-04-12 21:38:22 +0545

    final commit....

--- app/src/main/java/com/manishnumtowordapplication/MainActivity.java
+++ app/src/main/java/com/manishnumtowordapplication/MainActivity.java
@@ -2,12 +2,32 @@ package com.manishnumtowordapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvWords;
+    Button btnConvert;
+    EditText etNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etNumber= findViewById(R.id.etNumber);
+        tvWords= findViewById(R.id.tvWords);
+        btnConvert= findViewById(R.id.btnConvert);
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num;
+                num= Integer.parseInt(etNumber.getText().toString());
+                Manish_Algo manishAlgo = new Manish_Algo(num);
+                tvWords.setText(manishAlgo.Words());
+            }
+        });
     }
 }
